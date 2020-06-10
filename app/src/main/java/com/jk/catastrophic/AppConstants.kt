30dbc789ebce398d642e67f58401a8f0eeb71dfa
Jitendra Prajapati <jk2praj@gmail.com>
2020-06-10 12:06:46 +0530

    setup CI using github action
    * setup CI using github action
    
    * fix local.properties issue
    
    * fix local.properties issue (put in $)
    
    * fix local.properties issue (removed $secret)
    
    * fix local.properties issue (implemented full localproperties as secret)
    
    * fix local.properties issue (added clean before build)
    
    * fix local.properties issue (added / before properties file)
    
    * fix local.properties issue (added $Home before the secret)
    
    * fix local.properties issue (added "$LOCALPROPERTIEs" before the secret)
    
    * fix local.properties issue (created local.properties file using cat command)
    check for local.properties file in build.gradle
    updated gradle-wrapper.properties for new version 5.6.4
    
    * replaced &lt to >
    
    * replaced ENDOFFILE to EOF
    
    * replaced   >> EOF to <<EOF
    
    * passed whole data to secret key
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * passed EOF in Cat command
    
    * print using ls -al
    
    * print using ls -al
    
    * ./gradle assembleDebug
    
    * MINOR REFRACTORING
    
    * MINOR REFRACTORING

--- app/src/main/java/com/jk/catastrophic/AppConstants.kt
+++ app/src/main/java/com/jk/catastrophic/AppConstants.kt
@@ -1,5 +1,6 @@
 package com.jk.catastrophic
+import com.jk.catastrophic.BuildConfig.BASEURL
 
 object AppConstants {
-    const val BASE_URL = BuildConfig.BASEURL
+    const val BASE_URL = BASEURL
 }
